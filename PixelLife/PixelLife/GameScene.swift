55234a7afa9af07441ec34e010d7c5a8f638da94
Jack Jiang <shenghangjiang8715487@gmail.com>
2015-08-11 17:41:46 -0500

    1st commit

--- PixelLife/PixelLife/GameScene.swift
+++ PixelLife/PixelLife/GameScene.swift
@@ -9,37 +9,96 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    //script references
+    var craftingItemsReference: CraftingItems = CraftingItems()
+    
+    var classSelection: Int = 0
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        myLabel.text = "Select your character";
+        myLabel.fontSize = 35;
+        myLabel.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center;
+        myLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Left;
+        myLabel.position = CGPoint(x:CGRectGetMaxX(self.frame)/2, y:CGRectGetMaxY(self.frame)/2);
+        myLabel.name = "blacksmith"
+        myLabel.fontColor = SKColor.redColor()
         
-        self.addChild(myLabel)
+        //self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch in (touches as! Set<UITouch>) {
+            
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let nodesInTouch = self.nodesAtPoint(location)
+            let nodesInScene = self.children
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            for node in nodesInScene {
+                var nodeColor: SKLabelNode = node as! SKLabelNode
+                nodeColor.fontColor = SKColor.whiteColor()
+            }
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+           // self.addChild(sprite)
+            for node in nodesInTouch {
+                var nodeColor: SKLabelNode = node as! SKLabelNode
+                
+                if node.name == "blacksmith" {
+                    classSelection = 1
+                    
+                    nodeColor.fontColor = SKColor.redColor()
+                } else if node.name == "sorcerer" {
+                    classSelection = 2
+                    nodeColor.fontColor = SKColor.redColor()
+                } else if node.name == "carpenter" {
+                    classSelection = 3
+                    nodeColor.fontColor = SKColor.redColor()
+                } else if node.name == "research" {
+                    nodeColor.fontColor = SKColor.redColor()
+                    openResearchTree(classSelection)
+                } else if node.name == "craft" {
+                    nodeColor.fontColor = SKColor.redColor()
+                    openCrafting(classSelection)
+                }
+                
+            }
             
-            sprite.runAction(SKAction.repeatActionForever(action))
             
-            self.addChild(sprite)
+            
+            
         }
     }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func openResearchTree(selection :Int) {
+        if selection == 1 {
+            
+        } else if selection == 2 {
+            
+        } else if selection == 3 {
+            
+        }
+    }
+    func openCrafting(selection: Int) {
+        if selection == 1 {
+            for item in craftingItemsReference.blacksmithCrafting {
+                debugPrintln(item)
+            }
+        } else if selection == 2 {
+            for item in craftingItemsReference.sorcererCrafting {
+                debugPrintln(item)
+            }
+        } else if selection == 3 {
+            for item in craftingItemsReference.carpenterCrafting {
+                debugPrintln(item)
+            }
+        }
     }
 }
